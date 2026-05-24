4e1d2d2af2554706b87dab9404376d1c3f26ac4a
Gh0s7j0k3r <miguel.moises@ymail.com>
2026-05-24 20:54:21 -0300

    adiciona outras linguas para o hello world

--- hello.py
+++ hello.py
@@ -1,3 +1,35 @@
 #!/usr/bin/env python3
 
-print("Hello, World!")
+"""Hello world multi linguas.
+
+Dependendo da lingua configurada no ambiente, o programa exibe a mensagem 
+correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Miguel Moises"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "hello,World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+print(msg)
